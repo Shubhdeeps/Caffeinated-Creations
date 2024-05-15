00bff9737efa471c29224f7e2034efa4e0f9feb1
Shubhdeeps <shubhdeep86993@gmail.com>
2024-05-16 02:07:45 +0300

    init blogsphere

--- client/src/pages/Overview.page.tsx
+++ client/src/pages/Overview.page.tsx
@@ -1,9 +1,3 @@
-import Markdown from "@/features/Markdown";
-
 export default function OverviewPage() {
-  return (
-    <div className="p-2">
-      <Markdown />
-    </div>
-  );
+  return <div className="container border"></div>;
 }
