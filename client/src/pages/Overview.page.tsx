10827abf3d92006b65d39fb1bd64670a2f0d2d14
Shubhdeeps <shubhdeep86993@gmail.com>
2024-05-19 03:04:39 +0300

    overview layout init

--- client/src/pages/Overview.page.tsx
+++ client/src/pages/Overview.page.tsx
@@ -1,3 +1,8 @@
 export default function OverviewPage() {
-  return <div className="container border"></div>;
+  return (
+    <div className="container h-screen flex flex-row">
+      <div className="border w-full h-full flex-[1] p-3"></div>
+      <div className="border w-full h-full flex-[2.5] p-3"></div>
+    </div>
+  );
 }
